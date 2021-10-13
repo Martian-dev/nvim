638daa45b9c12aa5539f03c4e6aa33b15663f83d
Vaibhav P <52103891+vaibhav-75007@users.noreply.github.com>
2021-10-13 12:26:50 +0530

    Deleted coc-settings.json (i dont need it anymore) and imported lualine config in init.lua

--- init.lua
+++ init.lua
@@ -6,12 +6,12 @@ require('settings')
 require('mappings')
 vim.cmd("source $HOME/.config/nvim/vim-plug/plugins.vim")
 require('theme')
--- vim.cmd("source $HOME/.config/nvim/themes/airline.vim")
 vim.cmd("source $HOME/.config/nvim/plug-config/fzf.vim")
 vim.cmd("source $HOME/.config/nvim/plug-config/emmet.vim")
 require('treesitter-config')
 require('configs/blankline')
 require('configs/bufferline')
+require('configs/lualine')
 
 -- Native LSP
 require'lspconfig'.pyright.setup{}
